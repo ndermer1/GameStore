3ffeacd1e16d2fd7c39ef87ed86f091b58de58ee
Nedimnc <nedimnumanovicx@gmail.com>
2025-04-04 23:28:31 -0400

    Added  backend routes, Supabase config, and set up the server

--- backend/config/supabase.js
+++ backend/config/supabase.js
@@ -1,8 +1,11 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
 
-//retrieves url and key from .env
-const supabaseUrl = process.env.SUPABASE_URL; 
-const supabaseKey = process.env.SUPABASE_KEY; 
+dotenv.config(); // loads .env
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import productRoutes from "./routes/products.js";
+import cartRoutes from "./routes/cart.js"; 
+
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
